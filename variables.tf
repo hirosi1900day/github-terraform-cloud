cbaa14b2ecbd5d8890fe65f89ccb3cc7c8214785
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-04-23 19:23:04 +0900

    変数追加

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "GITHUB_APP_ID" {
+  description = "GITHUB_APP_ID"
+  type        = string
+}
+
+variable "GITHUB_APP_INSTALLATION_ID" {
+  description = "GITHUB_APP_INSTALLATION_ID"
+  type        = string
+}
+
+variable "GITHUB_APP_PEM_FILE" {
+  description = "GITHUB_APP_PEM_FILE"
+  type        = string
+}
